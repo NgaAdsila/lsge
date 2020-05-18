2ab423a65601309ac75b3022c18384532b65e2d3
Hà Quang Ngà <ngahq@kaopiz.com>
2020-05-18 11:50:17 +0700

    first commit

---
+++ src/main/java/my/lsge/LsgeApplication.java
@@ -0,0 +1,13 @@
+package my.lsge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/lsge/LsgeApplicationTests.java
@@ -0,0 +1,13 @@
+package my.lsge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
