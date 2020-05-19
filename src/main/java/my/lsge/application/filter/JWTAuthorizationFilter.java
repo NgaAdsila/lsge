a1d636db5be3e51ec55595bdd6ed12954f901a04
Hà Quang Ngà <ngahq@kaopiz.com>
2020-05-19 17:38:36 +0700

    config

--- src/main/java/my/lsge/application/filter/JWTAuthorizationFilter.java
+++ src/main/java/my/lsge/application/filter/JWTAuthorizationFilter.java
@@ -9,5 +9,4 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
         super(authenticationManager);
     }
 
-
 }
--- src/main/java/my/lsge/config/WebSecurityConfig.java
+++ src/main/java/my/lsge/config/WebSecurityConfig.java
@@ -3,14 +3,18 @@ package my.lsge.config;
 import lombok.extern.slf4j.Slf4j;
 import my.lsge.application.filter.ExceptionHandlerFilter;
 import my.lsge.application.filter.JWTAuthorizationFilter;
+import my.lsge.application.service.LoginUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,6 +26,9 @@ import java.util.Arrays;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoginUserDetailsService loginUserDetailsService;
 
     @Value("${app.security.non-auth-api}")
     private String[] nonAuthAPIs;
@@ -58,5 +65,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(loginUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 }
