e36e9a9c708fa9886738dfb1247ae9a3cda15f5d
ngahq <quangngabkit@gmail.com>
2020-06-11 00:10:35 +0700

    home page

--- web-client/src/pages/Home.vue
+++ web-client/src/pages/Home.vue
@@ -1,12 +1,43 @@
 <template>
-    <div class="home-page">
-        ABC
-    </div>
+    <MyCard
+            :cardInfo="cardInfo" />
 </template>
 
 <script>
+    import MyCard from "../components/home/MyCard";
     export default {
-        name: "Home"
+        name: "Home",
+        components: {MyCard},
+        data() {
+            return {
+                cardInfo: {
+                    header: this.$t('home.my_card.header'),
+                    contents: [
+                        {
+                            title: this.$t('home.my_card.content_title1'),
+                            content: this.$t('home.my_card.content1')
+                        },
+                        {
+                            title: this.$t('home.my_card.content_title2'),
+                            content: this.$t('home.my_card.content2')
+                        }
+                    ],
+                    sign: {
+                        title: this.$t('home.my_card.sign_title'),
+                        content: this.$t('home.my_card.sign')
+                    },
+                    footer: {
+                        detail: {
+                            label: this.$t('home.my_card.view_detail'),
+                            link: '/about_me'
+                        },
+                        socials: [
+
+                        ]
+                    }
+                }
+            }
+        }
     }
 </script>
 
